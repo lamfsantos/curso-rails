a1c36d3c8f2290cd054485673bdbfca5004a9ed2
Luiz André Furlan <lamfsantos5@gmail.com>
2023-01-28 15:54:57 -0400

    form para cadastrar usuário

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :contacts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -9,4 +8,7 @@ Rails.application.routes.draw do
   get 'sobre', to: 'static_pages#sobre'
 
   get 'contato', to: 'static_pages#contato'
+
+  resources :contacts
+  resources :users, only: [:new, :create]
 end
